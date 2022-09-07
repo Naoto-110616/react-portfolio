b329a2d8e883565ead894a8360d305ca3a1a0329
Naoto Okawa <siodeitadaku@gmail.com>
2022-09-08 04:27:31 +0900

    fix animation

--- reactapp/src/components/sideProjects/SideProjects.js
+++ reactapp/src/components/sideProjects/SideProjects.js
@@ -2,10 +2,42 @@ import SectionTitle from "../sectionTitle/SectionTitle";
 import classes from "./SideProjects.module.css";
 import SideProjectsItem from "./sideProjectsItem/SideProjectsItem";
 import projects from "../../common/projects";
+import { useRef } from "react";
+import gsap from "gsap";
+import { useEffect } from "react";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
 const SideProjects = () => {
-	const sideProject = projects.map((project,i) => (
-		<SideProjectsItem key={project.id} data={project} />
+	const sideProjectCardRefs = useRef([]);
+	const setAnimation = () => {
+		gsap.from(sideProjectCardRefs.current, 1, {
+			autoAlpha: 0,
+			y: +200,
+			duration: 0.5,
+			stagger: 0.1,
+			scrollTrigger: {
+				trigger: "#side-projects",
+				scrub: true,
+				start: "start center",
+				end: "end 10%",
+			},
+		});
+	};
+
+	useEffect(() => {
+		gsap.registerPlugin(ScrollTrigger);
+		setAnimation();
+	}, [sideProjectCardRefs]);
+
+	const sideProject = projects.map((project, i) => (
+		<div
+			ref={(element) => {
+				sideProjectCardRefs.current[i] = element;
+			}}
+			className={`${classes.sideProjectsItem}`}
+		>
+			<SideProjectsItem key={project.id} data={project} />
+		</div>
 	));
 	return (
 		<section id="side-projects" className={classes["side-projects"]}>
